8ec118c33ed7294a96d3ef1a550cba375e44d726
James Kyburz <james.kyburz@gmail.com>
2021-08-21 15:56:21 +0100

    fix: client size removed dependencies (shrunk from 491kb to 47kb)

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -1,20 +1,28 @@
 #!/usr/bin/env node
 
-const fetch = require('node-fetch')
+'use strict'
+
 const WebSocket = require('ws')
-const delay = require('delay')
+const http = require('http')
 const { fork } = require('child_process')
-const tempy = require('tempy')
-const touch = require('touch')
 const fs = require('fs')
-const { execSync } = require('child_process')
-const sleep = n => execSync(`sleep ${n}`)
+const os = require('os')
+const path = require('path')
 
-const { DEBUG_PROXY } = process.env
+const { DEBUG_PROXY, DEBUG_PID, DEBUG } = process.env
 
-if (!DEBUG_PROXY) throw new TypeError('missing DEBUG_PROXY')
+const env = process.env.__DEBUG_CHILD__
+  ? { ...process.env }
+  : {
+      __DEBUG_CHILD__: true,
+      __DEBUG_PROXY__: process.env.DEBUG_PROXY,
+      __DEBUG_PID__: process.env.DEBUG_PID,
+      __DEBUG_STDIO__: process.env.DEBUG ? 'inherit' : 'ignore'
+    }
 
-const stdio = process.env.DEBUG ? 'inherit' : 'ignore'
+if (!env.__DEBUG_PROXY__) throw new TypeError('missing DEBUG_PROXY')
+
+const stdio = env.__DEBUG_STDIO__
 
 if (process.env.__DEBUG__) {
   process.on('message', ({ pid }) => {
@@ -27,49 +35,45 @@ if (process.env.__DEBUG__) {
       process.exit(1)
     })
   } else {
-    const lockfile = tempy.file()
+    const lockfile = path.join(
+      os.tmpdir(),
+      `sandbox-debugger-lock-${Date.now().toString(32)}`
+    )
     const ps = fork(__filename, {
       env: {
-        ...process.env,
-        __DEBUG_PID__: process.env.DEBUG_PID || process.pid,
+        ...env,
+        __DEBUG_PID__: env.__DEBUG_PID__ || process.pid,
         __DEBUG_LOCK__: lockfile
       },
       stdio
     })
     ps.on('exit', () => process.exit(0))
-    const waitForLockfile = () => {
-      while (true) {
-        try {
-          fs.statSync(lockfile)
-          break
-        } catch (e) {}
-        sleep(1)
-      }
+    while (true) {
+      try {
+        fs.statSync(lockfile)
+        break
+      } catch (e) {}
+      blockingSleep(500)
     }
-    waitForLockfile()
   }
 }
 
 async function startDebug () {
   const ps = fork(__filename, {
-    env: { ...process.env, __DEBUG__: true },
+    env: { ...env, __DEBUG__: true },
     stdio
   })
   ps.send({ pid: process.env.__DEBUG_PID__ })
   ps.on('exit', () => process.exit(0))
-  const proxy = process.env.DEBUG_PROXY
+  const proxy = process.env.__DEBUG_PROXY__
+  const localEndpoint = 'http://localhost:9229'
 
   while (true) {
     try {
-      const getJSON = url => fetch(url).then(res => res.json())
-      const version = await getJSON('http://localhost:9229/json/version')
-      const json = await getJSON('http://localhost:9229/json')
+      const version = await getLocalDebugger(`${localEndpoint}/json/version`)
+      const json = await getLocalDebugger(`${localEndpoint}/json`)
       const session = JSON.stringify({ json, version })
-      await fetch(`http://${proxy}/session`, {
-        body: session,
-        method: 'POST'
-      })
-
+      await postSession(`http://${proxy}/session`, session)
       const remote = new WebSocket(`ws://${proxy}/session`)
       const local = new WebSocket(json[0].webSocketDebuggerUrl)
       const close = name => () => {
@@ -86,10 +90,14 @@ async function startDebug () {
       remote.once('close', close('remote'))
       remote.once('message', message => {
         remote.once('message', () => {
-          touch.sync(process.env.__DEBUG_LOCK__)
+          fs.writeFileSync(process.env.__DEBUG_LOCK__, '')
         })
-        remote.on('message', message => local.send(message.toString()))
-        local.on('message', message => remote.send(message.toString()))
+        remote.on('message', message => {
+          local.send(message.toString())
+        })
+        local.on('message', message => {
+          remote.send(message.toString())
+        })
 
         local.once('close', close('local'))
       })
@@ -99,4 +107,58 @@ async function startDebug () {
       await delay(100)
     }
   }
+}
+
+async function getLocalDebugger (url) {
+  const res = await new Promise((resolve, reject) => {
+    const req = http.get(url, res => {
+      res.on('error', reject)
+      res.setEncoding('utf8')
+      resolve(res)
+    })
+    req.on('error', reject)
+  })
+  if (res.statusCode !== 200) {
+    throw new Error(`${url} returned ${res.statusCode}`)
+  }
+  let data = ''
+  for await (const chunk of res) {
+    data += chunk.toString()
+  }
+  return JSON.parse(data)
+}
+
+async function postSession (url, postData) {
+  await new Promise((resolve, reject) => {
+    const req = http.request(
+      url,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': Buffer.byteLength(postData)
+        }
+      },
+      res => {
+        if (res.statusCode > 299) {
+          reject(new Error(`${url} returned ${res.statusCode}`))
+        } else {
+          res.on('error', reject)
+          res.on('data', () => null)
+          res.on('end', resolve)
+        }
+      }
+    )
+    req.on('error', reject)
+    req.write(postData)
+    req.end()
+  })
+}
+
+function blockingSleep (s) {
+  Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, Number(s / 1000))
+}
+
+function delay (ms) {
+  return new Promise(resolve => setTimeout(() => resolve(), ms))
 }
